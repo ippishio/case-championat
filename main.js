883086fb22f6f7cb6e98b28986d62ddebe99a4c0
ippishio <waterlava535@gmail.com>
2023-10-30 04:20:56 +0300

    huina

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
 
 
-const swiper = new Swiper('.swiper', {
-
-    direction: 'horizontal',
+const swiper_tinder = new Swiper('.swiper', {
+    direction: 'vertical',
     loop: true,
-  
-  
-  });
+    slidesPerView: 2,
+    spaceBetween : 100,
+    // effect: "coverflow",
+    // coverflowEffect: {
+    //     rotate: 15,
+    //     stretch: 25,
+    //     depth: 230,
+    //     modifier: 1.2,
+    //     slideShadows: false,
+    // },
+});
+
+const swiper = document.querySelector('.swiper').swiper
 var choosed_page = 1
-const button_color = "#46d460"
-const page_numbers = {1: "screen-home", 2:"screen-search", 3: "screen-tinder", 4: "screen-profile"}
+const button_color = "#f59f2f"
+const page_numbers = { 1: "screen-home", 2: "screen-search", 3: "screen-tinder", 4: "screen-profile" }
 document.getElementById("screen-home").style.visibility = "visible";
 document.getElementById("home").style.backgroundColor = button_color
-document.getElementById("home").addEventListener("click", function() {
+document.getElementById("home").addEventListener("click", function () {
     const buttons = document.querySelectorAll('.navbar-button');
     buttons.forEach(button => {
         button.style.backgroundColor = "#FFFFFF";
@@ -22,7 +31,7 @@ document.getElementById("home").addEventListener("click", function() {
     choosed_page = 1
     document.getElementById(page_numbers[choosed_page]).style.visibility = "visible";
 })
-document.getElementById("search").addEventListener("click", function() {
+document.getElementById("search").addEventListener("click", function () {
     const buttons = document.querySelectorAll('.navbar-button');
     buttons.forEach(button => {
         button.style.backgroundColor = "#FFFFFF";
@@ -32,7 +41,7 @@ document.getElementById("search").addEventListener("click", function() {
     choosed_page = 2
     document.getElementById(page_numbers[choosed_page]).style.visibility = "visible";
 })
-document.getElementById("profile").addEventListener("click", function() {
+document.getElementById("profile").addEventListener("click", function () {
     const buttons = document.querySelectorAll('.navbar-button');
     buttons.forEach(button => {
         button.style.backgroundColor = "#FFFFFF";
@@ -42,7 +51,7 @@ document.getElementById("profile").addEventListener("click", function() {
     choosed_page = 4
     document.getElementById(page_numbers[choosed_page]).style.visibility = "visible";
 })
-document.getElementById("tinder").addEventListener("click", function() {
+document.getElementById("tinder").addEventListener("click", function () {
     const buttons = document.querySelectorAll('.navbar-button');
     buttons.forEach(button => {
         button.style.backgroundColor = "#FFFFFF";
